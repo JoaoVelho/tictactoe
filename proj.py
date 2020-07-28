20e6e4708b7a3e57ba539101c2e7a5d47fff4c5b
Joao Velho <joaovictor.rv39@gmail.com>
2020-07-28 19:24:27 -0300

    works without the lines of the game

--- proj.py
+++ proj.py
@@ -4,69 +4,65 @@ import numpy as np
 def createPattern(path):
   img = cv2.resize(cv2.imread(path), (500, 500))
   imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
-  imgCanny = cv2.Canny(imgBlur, 50, 50)
+  imgCanny = cv2.Canny(imgGray, 50, 50)
 
   return imgCanny
 
 def getContours(img, patternX, patternO):
-  contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-  cntX, hierarchyX = cv2.findContours(patternX, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-  cntO, hierarchyO = cv2.findContours(patternO, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-  print(len(contours))
+  contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cntX, _ = cv2.findContours(patternX, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cntO, _ = cv2.findContours(patternO, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
   for cnt in contours:
     area = cv2.contourArea(cnt)
-    # print(area)
-    cv2.drawContours(imgContour, cnt, -1, (0, 0, 255), 3)
-    peri = cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, 0.01 * peri, True)
-    
-    resX = cv2.matchShapes(cntX[0], cnt, 3, 0.0)
-    print(resX)
-    resO = cv2.matchShapes(cntO[0], cnt, 3, 0.0)
-    print(resO)
+    print(area)
+    if area < 20000:
+      cv2.drawContours(imgContour, cnt, -1, (0, 0, 255), 3)
+      peri = cv2.arcLength(cnt, False)
+      approx = cv2.approxPolyDP(cnt, 0.01 * peri, False)
+      
+      resX = cv2.matchShapes(cntX[0], cnt, 3, 0.0)
+      # print(resX)
+      resO = cv2.matchShapes(cntO[0], cnt, 3, 0.0)
+      # print(resO)
 
-    x, y, w, h = cv2.boundingRect(approx)
-    if resX >= 0 and resX <= 0.3:
-      shape = 'Ex'
-      cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 0, 0), 2)
-      cv2.putText(
-        imgContour, 
-        shape, 
-        (x + (w // 2) - 10, y - 10), 
-        cv2.FONT_HERSHEY_COMPLEX, 
-        1, 
-        (0, 155, 0), 
-        2
-      )
-    elif resO >= 0 and resO <= 0.3:
-      shape = 'Circle'
-      cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 0, 0), 2)
-      cv2.putText(
-        imgContour, 
-        shape, 
-        (x + (w // 2) - 10, y - 10), 
-        cv2.FONT_HERSHEY_COMPLEX, 
-        1, 
-        (0, 155, 0), 
-        2
-      )
-
+      x, y, w, h = cv2.boundingRect(approx)
+      if resX >= 0 and resX <= 0.3:
+        shape = 'X'
+        cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(
+          imgContour, 
+          shape, 
+          (x + (w // 2) - 10, y - 10), 
+          cv2.FONT_HERSHEY_COMPLEX, 
+          1, 
+          (0, 155, 0), 
+          2
+        )
+      elif resO >= 0 and resO <= 0.3:
+        shape = 'O'
+        cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(
+          imgContour, 
+          shape, 
+          (x + (w // 2) - 10, y - 10), 
+          cv2.FONT_HERSHEY_COMPLEX, 
+          1, 
+          (0, 155, 0), 
+          2
+        )
   
-X = 'ex.png'
-O = 'circle.png'
+Xpath = 'ex1.png'
+Opath = 'circle.png'
 path = 'board.png'
 img = createPattern(path)
 imgContour = cv2.resize(cv2.imread(path), (500, 500))
+cv2.imshow('Original', imgContour)
 
-patternX = createPattern(X)
-patternO = createPattern(O)
+patternX = createPattern(Xpath)
+patternO = createPattern(Opath)
 
 getContours(img, patternX, patternO)
 
-cv2.imshow('Original', img)
-# cv2.imshow('Gray', imgGray)
-# cv2.imshow('Blur', imgBlur)
-# cv2.imshow('Canny', imgCanny)
 cv2.imshow('Contour', imgContour)
 cv2.waitKey(0)
