dd21f090dd69ece4a95b5b3a3d4971d604e46685
Joao Velho <joaovictor.rv39@gmail.com>
2021-06-15 20:10:13 -0300

    detect colors

--- proj.py
+++ proj.py
@@ -3,6 +3,18 @@ import numpy as np
 
 def createPattern(path):
   img = cv2.resize(cv2.imread(path), (500, 500))
+  imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+  lower = np.array([0,43,0])
+  upper = np.array([179,255,255])
+  mask = cv2.inRange(imgHSV,lower,upper)
+  imgResult = cv2.bitwise_and(img,img,mask=mask)
+  imgGray = cv2.cvtColor(imgResult, cv2.COLOR_BGR2GRAY)
+  imgCanny = cv2.Canny(imgGray, 200, 50)
+
+  return imgCanny
+
+def createPatternItem(path):
+  img = cv2.resize(cv2.imread(path), (500, 500))
   imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   imgCanny = cv2.Canny(imgGray, 200, 50)
 
@@ -54,16 +66,51 @@ def getContours(img, patternX, patternO):
   
 Xpath = 'ex1.png'
 Opath = 'circle.png'
-path = 'board5.jpg'
+path = 'boardred.jpg'
 img = createPattern(path)
 imgContour = cv2.resize(cv2.imread(path), (500, 500))
 cv2.imshow('Original', imgContour)
 cv2.imshow('Canny', img)
 
-patternX = createPattern(Xpath)
-patternO = createPattern(Opath)
+patternX = createPatternItem(Xpath)
+patternO = createPatternItem(Opath)
 
 getContours(img, patternX, patternO)
 
 cv2.imshow('Contour', imgContour)
+
+# def empty(a):
+#   pass
+
+# path = 'boardred.jpg'
+# cv2.namedWindow("TrackBars")
+# cv2.resizeWindow("TrackBars",640,240)
+# cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+# cv2.createTrackbar("Hue Max","TrackBars",179,179,empty)
+# cv2.createTrackbar("Sat Min","TrackBars",15,255,empty)
+# cv2.createTrackbar("Sat Max","TrackBars",234,255,empty)
+# cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
+# cv2.createTrackbar("Val Max","TrackBars",127,255,empty)
+
+# while True:
+#   img = cv2.resize(cv2.imread(path), (500, 500))
+#   imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+#   h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+#   h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+#   s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+#   s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+#   v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+#   v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+#   print(h_min,h_max,s_min,s_max,v_min,v_max)
+#   lower = np.array([h_min,s_min,v_min])
+#   upper = np.array([h_max,s_max,v_max])
+#   mask = cv2.inRange(imgHSV,lower,upper)
+#   imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+#   cv2.imshow("Original",img)
+#   cv2.imshow("HSV",imgHSV)
+#   cv2.imshow("Mask", mask)
+#   cv2.imshow("Result", imgResult)
+
+#   cv2.waitKey(1)
 cv2.waitKey(0)
